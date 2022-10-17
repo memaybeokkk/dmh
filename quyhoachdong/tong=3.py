cb6bcacd7eb09287d932f7ac934fa0d81a36bc87
memaybeokkk <115453068+memaybeokkk@users.noreply.github.com>
2022-10-17 08:20:11 +0700

    abx

--- quyhoachdong/tong=3.py
+++ quyhoachdong/tong=3.py
@@ -1,14 +1,11 @@
-a=[3,2,5,1,8,6,3,9]
-s1=sum(a)
+a=[3,2,5,1,8,6,3]
 a=['j']+a
-
+s1=14
 l=[0]*(s1+1)
 n=len(a)
-s1=14
 d=[0]*100000
-Sl=0
 def xuli():
-	global l,kt,Sl
+	global l,kt,d
 	s=s1
 	l=[0]*(s+1);tongmax=0;l[0]=1;kt=False
 	for i in range(1,n):
@@ -21,15 +18,18 @@ def xuli():
 						break
 					if st+a[i]>tongmax:
 						tongmax=st+a[i]
+	if kt==False:
+		return ()
 	while s>0:
 		i=l[s]
 		if i>=1:
 			print(a[i],end=' ')
 			d[i]=1
-			Sl+=1
 			s-=a[i]
-while Sl!=n:
+while True:
 	xuli()
 	print()
 	if kt==False:
 		break
+
+
